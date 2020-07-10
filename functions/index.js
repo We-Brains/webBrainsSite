ee19edf4be46427865b2d2e094113602dc6c77a5
MMesyats <mmesyatz@gmail.com>
2020-07-10 14:21:49 +0300

    feat(bot): add chat_id to firestore on start

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const functions = require('firebase-functions');
 const telegraf = require('telegraf');
 const admin = require('firebase-admin');
@@ -6,11 +8,45 @@ admin.initializeApp();
 const bot = new telegraf.Telegram(functions.config().bot.token);
 
 exports.addUser = functions.https.onRequest((req, res) => {
-	bot.onText(/\/start/, async (msg) => {
-		const chatId = msg.chat.id;
-		bot.sendMessage(chatId, 'Hello there');
-	});
-	res.status(200).send('ok');
+	const isTelegramMessage = req.body && req.body.message && req.body.message.chat && req.body.message.chat.id;
+
+	if (isTelegramMessage) {
+		const chat_id = req.body.message.chat.id;
+		const message = req.body.message.text;
+		if (message === '/start') {
+			admin.firestore().collection('/users').get().then((snapshot) => {
+				const chat_ids = snapshot.docs.map((doc) => doc.data().chat_id);
+				if (chat_ids.includes(`${chat_id}`)) {
+					return res.status(200).send({
+						method: 'sendMessage',
+						chat_id,
+						text: 'Добро пожаловать'
+					});
+				} else {
+					admin
+						.firestore()
+						.collection('/users')
+						.add({
+							chat_id: `${chat_id}`
+						})
+						.then(() => {
+							return res.status(200).send({
+								method: 'sendMessage',
+								chat_id,
+								text: 'Добро пожаловать'
+							});
+						})
+						.catch(() => {
+							return res.status(200).send({
+								method: 'sendMessage',
+								chat_id,
+								text: `Что то пошло не так при добавлении ид этого чата ${chat_id}`
+							});
+						});
+				}
+			});
+		}
+	}
 });
 
 exports.sendMail = functions.https.onRequest((req, res) => {
@@ -24,9 +60,9 @@ exports.sendMail = functions.https.onRequest((req, res) => {
 			chat_ids.forEach((chat_id) => {
 				bot
 					.sendMessage(chat_id, `Имя: ${data.name}\nТелефон: ${data.phone}`)
+					.then(() => res.status(200).send('ok'))
 					.catch(() => res.status(500).send('fail'));
 			});
-			res.status(200).send('ok');
 		})
 		.catch(() => {
 			res.send(500).send('fail');
